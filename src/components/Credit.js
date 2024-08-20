153798dd76f831cc5b4fcb76a33deb218bd1fca8
Zainnaveed <zainnaveed4307@gmail.com>
2024-08-20 15:11:53 +0500

    2nd

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -7,7 +7,7 @@ export default function Credit() {
       Coded by
       <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
         > Zain Naveed</a>, 
-        <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+        <a href="https://github.com/shemmee/React-Weather-App">Open sourced on Github.</a>
     </p>
   </div>
   );
